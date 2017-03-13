b85ea63cf5247ead6040a184116d1c805efa5dc8
PTC\odsptc8 <liliyan2801@gmail.com>
2017-03-13 16:34:17 +0200

    add test + gitignore

--- Algs/Algs/test.cpp
+++ Algs/Algs/test.cpp
@@ -1,9 +1,29 @@
+#include <iostream>
+
 int search(int A[], int size, int key)
 {
-	for (int i = 0; i < size; i++)
+	int i = 0;
+	while(i < size)	
 	{
 		if (A[i] == key)
 			return i;
+		i++;
 	}
 	return -1;
+}
+ 
+void test(int A[], int size)
+{
+	std::cout << ((search(A, size, 1) == 0)    ? "passed" : "failed") << std::endl;
+	std::cout << ((search(A, size, 0) == -1)   ? "passed" : "failed") << std::endl;
+	std::cout << ((search(A, size, 'd') == -1) ? "passed" : "failed") << std::endl;
+	std::cout << ((search(A, size, 2) == 1)    ? "passed" : "failed") << std::endl;
+	std::cout << ((search(A, size, 100) == -1) ? "passed" : "failed") << std::endl;
+}
+
+int main()
+{
+	int array[] = { 1, 2, 3, 4, 5 };
+	test(array, 5);
+	return 0;
 }
