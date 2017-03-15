bae70786f0fabd8f839f0182836671ca850c5c61
Nikolenko <LNikolenko@kiev.luxoft.com>
2017-03-15 11:27:10 +0200

    v3

--- Algs/Algs/test.cpp
+++ Algs/Algs/test.cpp
@@ -4,16 +4,21 @@ using namespace std;
 
 ostream& operator<<(ostream& o, const vector<T>& v)
 {
-	
+	o << "[ ";
+	for(auto&x : v)
+	{
+		o << x << ", ";
+	}
 	return o << " ]";
 }
+
 template<class TFunc, class TResult, class TParam1>
 void test(TResult expect, TFunc f, TParam1 p1)
 {
 	auto got = f(p1);
 	if (got != expect)
 	{
-		cerr << "failed: " << expect << " != " << got;
+		cerr << "failed: " << expect << " != " << got << endl;
 	}
 }
 
@@ -23,11 +28,36 @@ void test(TResult expect, TFunc f, TParam1 p1, TParam2 p2)
 	auto got = f(p1, p2);
 	if (got != expect)
 	{
-		cerr << "failed: f(" << p1 << ", " << p2 << ") = " expect != got;
+		cerr << "failed: f(" << p1 << ", " << p2 << ") = " got << "!=" << expect << endl;
+	}
+	else
+	{		
+		cout << "passed";
 	}
 }
 
-int search_1(const std::vector<int>& v, int key)
+int search_0(int v[], size_t size, int key) 
+{
+    for (int i = 0; i < size; ++i)
+	{
+        if (v[i] == key){ return i; }
+    }
+    return -1;
+}
+
+int search_1(int v[], size_t size, int key)
+{
+    v[size] = key;
+    int i = 0;
+    while (v[i] != key) { ++i; }
+    if (i != size)
+	{ 
+        return i;
+    }
+    return -1;
+}
+
+int search_2(const std::vector<int>& v, int key)
 {
 	for(int i = 0; i < v.size(); ++i)
 	{
@@ -39,7 +69,7 @@ int search_1(const std::vector<int>& v, int key)
 	return -1;
 }
 
-int search(std::vector<int>& v, int key)
+int search_3(std::vector<int>& v, int key)
 {
 	v.push_back(key);
 	int i = 0;
@@ -55,38 +85,19 @@ int search(std::vector<int>& v, int key)
 	return -1;
 }
  
-void test(const std::vector<int>& v)
-{
-	std::cout << ((search(v, 1) == 0)    ? "passed" : "failed") << std::endl;
-	std::cout << ((search(v, 0) == -1)   ? "passed" : "failed") << std::endl;
-	std::cout << ((search(v, 'd') == -1) ? "passed" : "failed") << std::endl;
-	std::cout << ((search(v, 2) == 1)    ? "passed" : "failed") << std::endl;
-	std::cout << ((search(v, 100) == -1) ? "passed" : "failed") << std::endl;
-}
-
-void test(std::vector<int>& v)
-{
-	std::cout << ((search(v, 1) == 0) ? "passed" : "failed") << std::endl;
-	std::cout << ((search(v, 0) == -1) ? "passed" : "failed") << std::endl;
-	std::cout << ((search(v, 'd') == -1) ? "passed" : "failed") << std::endl;
-	std::cout << ((search(v, 2) == 1) ? "passed" : "failed") << std::endl;
-	std::cout << ((search(v, 100) == -1) ? "passed" : "failed") << std::endl;
-}
-
 void test_search()
 {
 	typedef vector<int> Array;
-	test(8, search, Array({ 0, 1, 2, 3, 4, 5, 6, 7, 8 }), 8);
+	test(8, search_2, Array({ 0, 1, 2, 3, 4, 5, 6, 7, 8 }), 8);
+	test(0, search_2, Array({ 0, 1, 2, 3, 4, 5, 6, 7, 8 }), 0);
+	test(-1, search_2, Array({ 0, 1, 2, 3, 4, 5, 6, 7, 8 }), 9);
+	test(-1, search_2, Array({ 0, 1, 2, 3, 4, 5, 6, 7, 8 }), -1);
+	test(0, search_2, Array({ 0}), 0);
+	test(-1, search_2, Array({ 0}), 1);
 }
 
 int main()
 {
-	std::vector<int> v;
-	int size = 5;
-	for (int i = 0; i < size; i++)
-	{
-		v.push_back(i+1);
-	}
-	test(v);
+	test_search();
 	return 0;
 }
