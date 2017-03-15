609565d242ea0d52b908d4a4d1355d86f7b21d31
LNikolenko <liliyan2801@gmail.com>
2017-03-15 17:42:39 +0200

    v3:

--- Algs/Algs/test.cpp
+++ Algs/Algs/test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 ostream& operator<<(ostream& o, const vector<T>& v)
 {
 	o << "[ ";
-	for(auto&x : v)
+	for (auto&x : v)
 	{
 		o << x << ", ";
 	}
@@ -31,40 +31,40 @@ void test(TResult expect, TFunc f, TParam1 p1, TParam2 p2)
 		cerr << "failed: f(" << p1 << ", " << p2 << ") = " got << "!=" << expect << endl;
 	}
 	else
-	{		
+	{
 		cout << "passed";
 	}
 }
 
-int search_0(int v[], size_t size, int key) 
+int search_0(int v[], size_t size, int key)
 {
-    for (int i = 0; i < size; ++i)
+	for (int i = 0; i < size; ++i)
 	{
-        if (v[i] == key){ return i; }
-    }
-    return -1;
+		if (v[i] == key) { return i; }
+	}
+	return -1;
 }
 
 int search_1(int v[], size_t size, int key)
 {
-    v[size] = key;
-    int i = 0;
-    while (v[i] != key) { ++i; }
-    if (i != size)
-	{ 
-        return i;
-    }
-    return -1;
+	v[size] = key;
+	int i = 0;
+	while (v[i] != key) { ++i; }
+	if (i != size)
+	{
+		return i;
+	}
+	return -1;
 }
 
 int search_2(const std::vector<int>& v, int key)
 {
-	for(int i = 0; i < v.size(); ++i)
+	for (int i = 0; i < v.size(); ++i)
 	{
 		if (v[i] == key)
 		{
 			return i;
-		}					
+		}
 	}
 	return -1;
 }
@@ -81,10 +81,10 @@ int search_3(std::vector<int>& v, int key)
 	if (i != v.size())
 	{
 		return i;
-	}	
+	}
 	return -1;
 }
- 
+
 void test_search()
 {
 	typedef vector<int> Array;
@@ -92,8 +92,8 @@ void test_search()
 	test(0, search_2, Array({ 0, 1, 2, 3, 4, 5, 6, 7, 8 }), 0);
 	test(-1, search_2, Array({ 0, 1, 2, 3, 4, 5, 6, 7, 8 }), 9);
 	test(-1, search_2, Array({ 0, 1, 2, 3, 4, 5, 6, 7, 8 }), -1);
-	test(0, search_2, Array({ 0}), 0);
-	test(-1, search_2, Array({ 0}), 1);
+	test(0, search_2, Array({ 0 }), 0);
+	test(-1, search_2, Array({ 0 }), 1);
 }
 
 int main()
